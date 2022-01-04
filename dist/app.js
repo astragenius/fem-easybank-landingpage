e0f87d41a4caa5ed432ca728305574f575477b62
astragenius <darthmalak86@gmail.com>
2022-01-04 20:36:10 +0100

    added hover effect to nav__mobile links.

--- dist/app.js
+++ dist/app.js
@@ -1,9 +1,11 @@
 const mobileMenu = document.getElementById('mobileMenu');
 const navMobile = document.getElementById('nav__mobile');
 const overlay = document.getElementById('overlay');
+const body = document.getElementById('body');
 
 mobileMenu.addEventListener('click', () => {
     mobileMenu.classList.toggle('open');
     navMobile.classList.toggle('slideOpen');
     overlay.classList.toggle('open');
+    body.classList.toggle('hide');
 })
