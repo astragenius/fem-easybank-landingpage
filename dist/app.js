9375e12375c826efb023b1f6928d7778a7d4e3e7
astragenius <darthmalak86@gmail.com>
2022-01-04 19:52:43 +0100

    Added Javascript functionallity to the mobile navigation. Toggle function for (overlay and show).

--- dist/app.js
+++ dist/app.js
@@ -1,5 +1,9 @@
 const mobileMenu = document.getElementById('mobileMenu');
+const navMobile = document.getElementById('nav__mobile');
+const body = document.getElementById('body');
 
 mobileMenu.addEventListener('click', () => {
     mobileMenu.classList.toggle('open');
+    navMobile.classList.toggle('slideOpen');
+    body.classList.toggle('overlay');
 })
